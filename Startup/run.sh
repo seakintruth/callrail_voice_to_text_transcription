17f0830f8d54dd7aae07ae6ae7e262f07dd0acff
Jeremy D. Gerdes <seakintruth@gmail.com>
2021-05-21 00:50:44 -0400

    run.sh paths to relative

--- Startup/run.sh
+++ Startup/run.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # run the R script
-/usr/bin/Rscript ~/git/callrail_voice_to_text/R/GetCalls.R
+/usr/bin/Rscript ../R/GetCalls.R
 # if this script can't find credentials it uses the script LoadApiKey.R
 
 # run the python scripts
-python3 ~/git/callrail_voice_to_text/python/convert_mp3_to_wav.py
-python3 ~/git/callrail_voice_to_text/python/convert_wav_to_text.py
+python3 ../python/convert_mp3_to_wav.py
+python3 ../python/convert_wav_to_text.py
 
 # run the last R script
-/usr/bin/Rscript ~/git/callrail_voice_to_text/R/LoadText.R
+/usr/bin/Rscript ../R/LoadText.R
 
-cd ~/git/callrail_voice_to_text
+cd ../
 
 # push updates to github
 git add . && git commit -m "data updates" && git push origin HEAD
